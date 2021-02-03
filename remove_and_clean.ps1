f391681b731342884416e9e85d38d932b9908ff7
Ville Puisto <60825081+VillePuisto@users.noreply.github.com>
2021-02-03 21:37:28 +0200

    Update remove_and_clean.ps1
    Kansion poistaminen

--- remove_and_clean.ps1
+++ remove_and_clean.ps1
@@ -1,3 +1,10 @@
 Write "Hello"
 Write "Tapio"
 Write "Tapio = BOOMER"
+
+# 1 Mene kansioon jonka sisallon haluat poistaa
+Set-Location "Kansion polku"
+
+# Poista kansio missa olet (kaikki)
+Remove-Item * -Recurse -force
+
